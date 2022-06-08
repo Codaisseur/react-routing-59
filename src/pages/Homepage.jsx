4085b57e2e08521858dea349fb81449b510bcee5
matiasgarcia91 <matiasigarcia91@gmail.com>
2022-06-08 10:26:25 +0200

    a different change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -36,7 +36,7 @@ const Homepage = () => {
 
   return (
     <div>
-      <h1>Welcome to the homepage!!</h1>
+      <h1>I wanted to change it in a different way!!</h1>
       <input
         type="text"
         value={term}
